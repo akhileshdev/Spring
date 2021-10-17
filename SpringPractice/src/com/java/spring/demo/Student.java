c64f9b7eefbf3a5905aeb718067eafcee081d90d
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-10-17 19:14:52 +0530

    @spring revision demo

---
+++ SpringPractice/src/com/java/spring/demo/Student.java
@@ -0,0 +1,21 @@
+package com.java.spring.demo;
+
+public class Student {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void display()
+	{
+		System.out.println("Hello : "+name);
+	}
+	
+	
+}
