bd5042e43b9d692f008ca94d5b57dec110680acc
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-10-18 22:19:30 +0530

    @spring revision CI with collection Object

---
+++ SpringPractice/src/com/java/spring/ci/collection/dependent/Question.java
@@ -0,0 +1,5 @@
+package com.java.spring.ci.collection.dependent;
+
+public class Question {
+
+}
---
+++ SpringPractice/src/com/java/spring/ci/collection/dependent/QuestionApplTest.java
@@ -0,0 +1,10 @@
+package com.java.spring.ci.collection.dependent;
+
+public class QuestionApplTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
