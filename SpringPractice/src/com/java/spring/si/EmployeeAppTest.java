cac3a4de6550f25adff02e261fc4763bf73fd048
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-10-20 23:21:45 +0530

    @spring revision SI basic

--- SpringPractice/src/com/java/spring/si/EmployeeAppTest.java
+++ SpringPractice/src/com/java/spring/si/EmployeeAppTest.java
@@ -1,16 +1,16 @@
-package com.java.spring.bean.inheritance;
+package com.java.spring.si;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class BeanInheritanceApplTest {
+public class EmployeeAppTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
-		Employee employee = context.getBean("employeebean", Employee.class);
-		
-		employee.display();		
+		Employee emp = context.getBean("employeebean",Employee.class);
+		System.out.println(emp);
 	}
 
 }
