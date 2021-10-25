e31cda63f09171800d44c9ecddfe42e8b6d74f61
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-10-25 23:23:49 +0530

    @Spring revision DI setter with Map

---
+++ SpringPractice/src/com/java/spring/si/map/Employee.java
@@ -0,0 +1,5 @@
+package com.java.spring.si.map;
+
+public class Employee {
+
+}
---
+++ SpringPractice/src/com/java/spring/si/map/EmployeeAppTest.java
@@ -0,0 +1,14 @@
+package com.java.spring.si.map;
+
+public class EmployeeAppTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		
+	}
+
+}
