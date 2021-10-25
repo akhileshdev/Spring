06fc37f13d191053e57a5bb57c1863c9e75c46a6
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-10-25 23:54:15 +0530

    @Spring revision DI setter with Map

--- SpringPractice/src/com/java/spring/si/map/Employee.java
+++ SpringPractice/src/com/java/spring/si/map/Employee.java
@@ -1,5 +1,35 @@
 package com.java.spring.si.map;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Employee {
 
+	private int id;
+	private String name;
+	private Map<String,String> address = new HashMap<String,String>();
+		
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Map<String, String> getAddress() {
+		return address;
+	}
+	public void setAddress(Map<String, String> address) {
+		this.address = address;
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", address=" + address + "]";
+	}	
 }
--- SpringPractice/src/com/java/spring/si/map/EmployeeAppTest.java
+++ SpringPractice/src/com/java/spring/si/map/EmployeeAppTest.java
@@ -1,14 +1,18 @@
 package com.java.spring.si.map;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class EmployeeAppTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");	
 		
+	Employee emp = context.getBean("employeebean",Employee.class);	
 		
-		
-		
+	System.out.println(emp);
+	
 	}
 
 }
